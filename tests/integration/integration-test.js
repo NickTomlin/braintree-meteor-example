2a92426f84e476a94e320a5e6b0ba1ec6bb907f4
Nick Tomlin <nick.tomlin@gmail.com>
2015-01-17 11:21:19 -0600

    Break form filling into helper functions

--- tests/integration/integration-test.js
+++ tests/integration/integration-test.js
@@ -1,33 +1,11 @@
 'use strict';
 
+var helpers = require('./helpers');
+
 // do not expect angular to be on page
 browser.ignoreSynchronization = true;
 
 describe('Braintree Meteor Example', function () {
-  var validCC = '4111 1111 1111 1111';
-  var validExp = '0916';
-
-  var validInputValues = {
-    firstName: 'Bob',
-    lastName: 'Bibbins'
-  };
-  var validSelectValues = {
-    tshirt: 'small',
-    quantity: 2
-  };
-
-  // TODO, this could use a cleanup
-  // and/or a smarter approach than
-  // sleep
-  function fillOutPaymentForm() {
-    browser.driver.sleep(10000);
-    browser.driver.switchTo().frame(0);
-    browser.driver.sleep(10000);
-
-    browser.driver.findElement(protractor.By.name('credit-card-number')).sendKeys(validCC);
-    browser.driver.findElement(protractor.By.name('expiration')).sendKeys(validExp);
-    browser.switchTo().defaultContent();
-  }
 
   it('loads a meteor application', function () {
     browser.get('/');
@@ -35,22 +13,10 @@ describe('Braintree Meteor Example', function () {
   });
 
   it('Processes a credit card transaction', function () {
+    helpers.fillOutOrderDetails();
+    helpers.fillOutPaymentForm();
+    helpers.completeCheckout();
 
-    for (var key in validInputValues) {
-      var value = validInputValues[key];
-      $('[name="' + key + '"]').sendKeys(value);
-    }
-
-    for (var key in validSelectValues) {
-        var value = validSelectValues[key];
-       element(by.cssContainingText('select', value)).click();
-    }
-
-    fillOutPaymentForm();
-
-    $('button[type="submit"]').click();
-
-    browser.driver.sleep(8000);
     expect($('h2').getText()).toEqual('Order Complete');
   });
 });
